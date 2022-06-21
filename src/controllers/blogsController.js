4e6096c672f2e3d498903792c357f8f483a9c122
SonuKumarYadav9 <skumaryadav.man@gmail.com>
2022-06-21 21:55:19 +0530

    deleteBlog

--- src/controllers/blogsController.js
+++ src/controllers/blogsController.js
@@ -1,14 +1,64 @@
 const blog = require('../models/blogModel');
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
+const blogModel = require('../models/blogModel');
 
-const createBlog = async function(req, res) {
+
+const getBooks = async function (req, res) {
     try {
-    let getBlogData = req.body;
-    let savedBlogData = await blog.create(getBlogData);
-    res.status(201).send({status: true, data: savedBlogData});
+        let getBlogData = req.body;
+        if (Object.keys(getBlogData).length == 0)
+            return res.status(400).send({ status: false, msg: "Data is required to create a blog" });
+
+        // checking that the below data is present or not
+        let { title, body, authorId, category } = getBlogData;   // Destucturing data
+        if (!title) return res.status(400).send({ status: false, msg: "Title must be filled" });
+        if (!body) return res.status(400).send({ status: false, msg: "Body must be filled" });
+
+        let getAuthorData = await author.findById(getBlogData.authorId);
+        console.log(authorId);
+        if (!getAuthorData) return res.status(404).send({ status: false, msg: "No such author exists" });
+        if (!category) return res.status(400).send({ status: false, msg: "Category must be present" });
+        let savedBlogData = await blog.create(getBlogData);
+        res.status(201).send({ status: true, data: savedBlogData });
     } catch (err) {
-        res.status(500).send({status: false, msg: err.message});
+        res.status(500).send({ status: false, msg: err.message });
     }
 };
 
-module.exports.createBlog = createBlog;
+//   const updatedBlog = async function (req,res){
+//        try {
+//          let blog = req.body
+//          let {title,body ,tags,subcategory} = blog
+
+
+//         //  if (!blog) return res.status(400).send ("Blog Not Found")
+
+
+//         //  let blog = await blogModel.findById(blogId)
+//        } catch (error) {
+
+//        }
+
+//   }
+
+// 51 to 53 line
+const deletedBlog = async function (req, res) {
+    try {
+        let params = req.params          // reqest from params to blogId
+        let blogId = params.blogId
+        if (Object.keys(blogId).length == 0)
+            return res.status(400).send({ status: false, msg: "Blog Id Not Found" });
+        // find By ID blog is present or not
+        let blog = await blogModel.findById({ _id: blogId, isDeleted: false, deletedAt: null })
+        if (!blog) return res.status(404).send({ status: false, msg: "Blog Not Found" })
+        console.log(blog)
+        res.status(200).send({status: true  msg: "Blg deleted Succesfully"})
+    } catch (err) {
+        res.status(500).send({ status: false, msg: err.message });
+    }
+};
+
+
+module.exports.createBlog = createBlog
+module.exports.getBooks = getBooks
+module.exports.deletedBlog = deletedBlog
--- src/routes/route.js
+++ src/routes/route.js
@@ -4,11 +4,13 @@ const authorController = require("../controllers/authorController");
 const blogController = require("../controllers/blogsController");
 
 router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
+    res.send("My first ever Api to Check Working Or Not !")
 })
 
 router.post("/createAuthor", authorController.createAuthor);
 
 router.post("/createBlog", blogController.createBlog);
+
+router.delete("/blogs:blogId", blogController.deletedBlog);
 
 module.exports = router;
