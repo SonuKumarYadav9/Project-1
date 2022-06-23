3d254ba7d9b79cfd84896ec7d8c61150a54f2b2c
Rahul Kumar Barnwal <78400704+rahulgithub-web@users.noreply.github.com>
2022-06-23 18:36:47 +0530

    updated everything

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,15 +2,28 @@ const express = require('express');
 const router = express.Router();
 const authorController = require("../controllers/authorController");
 const blogController = require("../controllers/blogsController");
+const middleware = require("../middlewares/auth")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever Api to Check Working Or Not !")
-})
-
+let { authentication, authorization } = middleware;
+// ---------- Create Author Api ---------
 router.post("/createAuthor", authorController.createAuthor);
 
-router.post("/createBlog", blogController.createBlog);
+// ---------- Login Author Api -----------
+router.post("/login", authorController.loginAuthor);
 
-router.delete("/blogs:blogId", blogController.deletedBlog);
+// ---------- Create Blog Api ------------ 
+router.post("/createBlog", authentication ,blogController.createBlog);
+
+// ---------- Get Blogs Api -------------
+router.get("/getBlogs", authentication ,blogController.getBlogs);
+
+// ------- Get Updated Blogs using blogId ------
+router.put("/getBlogs/:blogId", authentication, authorization ,blogController.updatedBlogs);
+
+// ---------- Delete Api ---------------
+router.delete("/blogs:blogId", authentication, authorization, blogController.deletedBlog);
+
+// ---------- Delete Blogs By Query ----------
+router.delete("/blogs", blogController.deletedByQueryParams);
 
 module.exports = router;
