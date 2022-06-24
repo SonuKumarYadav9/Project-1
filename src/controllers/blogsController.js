6f7b1b475acfed7e62ed1214820e8e65a508a4ca
Rahul Kumar Barnwal <78400704+rahulgithub-web@users.noreply.github.com>
2022-06-24 13:08:09 +0530

    change in updatedBlogs

--- src/controllers/blogsController.js
+++ src/controllers/blogsController.js
@@ -53,41 +53,36 @@ const getBlogs = async (req, res) => {
 
 const updatedBlogs = async function (req, res) {
   try {
-    let blogId = req.params.blogId
-    let blogs = await blog.findById(blogId)
-    console.log(blogs)
-    if (!blogs) {
-      return res.status(404).send({ status: false, msg: "No such blog exists " })
-    };
-    let isDelete = blogs.isDeleted;
-    if (isDelete == true) {
-      return res.status(200).send({ msg: true, data: "blog document is already deleted" })
-    }
+      let id = req.params.blogId
+      let data = req.body
+      let {title, body, subCategory, tags} = data;
+      let updateQuery = { title: title, body: body, isPublished: true, publishedAt: new Date() }
+      let addQuery = { tags: tags, subCategory: subCategory }
+      let blogData = await blog.findById(id)
+      let keys = Object.keys(data) 
+      let temp = 0;
+      //when no body is given
+      if (keys.length === 0)
+          return res.status(400).send({ status: false, msg: "No Data is given for updation" })
 
-    let body = req.body
-    let published = blog.isPublished
-    console.log(published)
-    if (published == true && Object.keys(body) != 0) {
+      //when query parametrs have attributes other than authorId,body,tags or subcategory
+      for (let i = 0; i < keys.length; i++) {
+          if (keys[i] == "title" || keys[i] == "body" || keys[i] == "tags" || keys[i] == "subCategory")
+              temp++;
+      }
+      if (keys.length > 0 && temp !== keys.length)
+          return res.status(400).send({ status: false, msg: "Invalid Request!! Query cannot have attribute other than title, body, tags or subcategory" })
 
-      let result = await blog.findOneAndUpdate({ _id: blogId }, body, { new: true })
-      console.log(result)
-      res.status(200).send({status: true, data: result })
-    }
-    else if (published == true && Object.keys(body) == 0) {
-      res.status(400).send({ msg: "already published" })
-    } else if (published == authorIdfalse && Object.keys(body) != 0) {
-      let result = await blog.findOneAndUpdate({ _id: blogId }, {$set: {isDeleted: true}, publishedAt: Date.now() }, { new: true })
-      console.log(result)
-      res.send({ data: result })
-    } else {
-      let result = await blog.findOneAndUpdate({ _id: blogId }, body, { new: true })
-      console.log(result)
-      res.status(200).send({status: true, data: result })
-    }
+      
+      if (blogData.isDeleted)
+          return res.status(404).send({ status: false, msg: "Blog is already Deleted" })
+
+      let getData = await blog.findOneAndUpdate({ _id: id }, { $set: updateQuery, $push: addQuery }, { new: true, upsert: true })
+
+      res.status(200).send({ status: true, data: getData })
   }
-  catch (err) {
-    console.log(err.message)
-    res.status(500).send({ msg: "error", error: err.message })
+  catch (error) {
+      res.status(500).send({ status: false, msg: error.message })
   }
 }
 
