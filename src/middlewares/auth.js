4e11c6b607fe30b9fc99965104d3ed7fa32217bd
Sonu Kumar Yadav <106573507+SonuKumarYadav9@users.noreply.github.com>
2022-06-25 17:03:34 +0530

    Authentication Update
    Sonu

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,18 +4,19 @@ const jwt = require('jsonwebtoken');
 const authentication = async function (req, res, next) {
     try {    ///// check token 
         let token = req.headers["x-api-key"]
-        if (!token) req.headers["x-api-key"]
-        if (!token) return res.status(404).send({ status: false, msg: "Token Must be Filled" })
-        console.log(token);
-        // verify token
-        let decodedToken = jwt.verify(token, "functionUp-project1");
-        if (!decodedToken) return res.status(400).send({ status: false, msg: "Token Not Verified Please Enter Valid Token" })
-        // find Author
-        let authorId = req.params.authorId
-        if(!authorId)  return res. status(404).send({status:false, msg:"Author Not Found"})
-        let authorDetail = await author.findById(authorId);
-        if (!authorDetail) return res.status(404).send({ status: false, msg: "Author Detail Not exist  " })
-     res.status(200).send({status: true, msg:"Author Authenticated Succesfully "})
+            if (!token) req.headers["x-api-key"]
+              if (!token) return res.status(400).send({ status: false, msg: "Token Must be Filled" })
+                console.log(token);
+          // verify token
+    let decodedToken = jwt.verify(token, "functionUp-project1");
+if (!decodedToken) return res.status(400).send({ status: false, msg: "Token Not Verified Please Enter Valid Token" })
+                            // find Author  by AuthorId
+                                            // let authorId = req.query.authorId
+                                                // if(!authorId)  return res. status(404).send({status:false, msg:"Author Not Found"})
+                                                   //   // find author detail fron authorId
+                                                        // let authorDetail = await author.findById(authorId);
+                                                               // if (!authorDetail) return res.status(404).send({ status: false, msg: "Author Detail Not exist  " })
+                                                                     //   return res.status(200).send({status: true, msg:"Author Authenticated Succesfully "})
      next();
     }
     catch (err) {
