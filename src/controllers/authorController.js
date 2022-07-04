4e9bcececb5bcd1248a4e872e22125e7702bf147
Sonu Kumar Yadav <106573507+SonuKumarYadav9@users.noreply.github.com>
2022-07-04 13:59:36 +0530

    new updated
    ss

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -1,71 +1,79 @@
-const author = require('../models/authorModel');
+    const author = require('../models/authorModel');
 
-const jwt = require("jsonwebtoken");
-const validateEmail = require('email-validator');
+                       const jwt = require("jsonwebtoken");
+
+                       const validateEmail = require('email-validator');
+
+
+//===================================CREATE AUTHOR===========================================
+
 
 const createAuthor = async function(req, res) {
     try {
-    let getAuhorData = req.body;
-    let {fName, lName, title, email, password} = getAuhorData;
-    if(!fName) return res.status(400).send({status: false, msg: "fName is missing"});
-    if(fName == 0) {
-        return res.status(400).send({status: false, msg: "Enter authors First Name"});
-    }
-    if(!lName) return res.status(400).send({status: false, msg: "lName is missing"});
-    if(lName == 0) {
-        return es.status(400).send({status: false, msg: "Enter authors Last Name"});
-    }
-    if(!title) return res.status(400).send({status: false, msg: "title is missing"});    
-    if(title == 0) { 
-        return res.status(400).send({status: false, msg: "Enter title of the book"});  
-    }  
-    if(!email) return res.status(400).send({status: false, msg: "Email is missing"});
-    if(email == 0) {
-        return res.status(400).send({ status: false, msg: "Enter a valid email id"});
-    }
-    if(!validateEmail.validate(req.body.email)) return res.status(400).send({ status: false, msg: "Enter a valid email "}); 
-    req.body.email = req.body.email.toLowerCase();
+       let getAuhorData = req.body;
+          let {fName, lName, title, email, password} = getAuhorData;
+             if(!fName) return res.status(400).send({status: false, msg: "fName is missing"});
+                if(fName == 0) {
+                   return res.status(400).send({status: false, msg: "Enter authors First Name"});
+                    }
+                      if(!lName) return res.status(400).send({status: false, msg: "lName is missing"});
+                       if(lName == 0) {
+                         return es.status(400).send({status: false, msg: "Enter authors Last Name"});
+                           }
+                           if(!title) return res.status(400).send({status: false, msg: "title is missing"});    
+                          if(title == 0) { 
+                       return res.status(400).send({status: false, msg: "Enter title of the book"});  
+                      }  
+                    if(!email) return res.status(400).send({status: false, msg: "Email is missing"});
+                  if(email == 0) {
+                 return res.status(400).send({ status: false, msg: "Enter a valid email id"});
+                }
+             if(!validateEmail.validate(req.body.email)) return res.status(400).send({ status: false, msg: "Enter a valid email "}); 
+           req.body.email = req.body.email.toLowerCase();
 
-    if(!password) return res.status(400).send({status: false, msg: "password is missing"});
+         if(!password) return res.status(400).send({status: false, msg: "password is missing"});
     if(password == 0) {
         return res.status(400).send({ status: false, msg: "Enter a valid password" });
     }                                 
     let savedAuhorData = await author.create(getAuhorData);
-    res.status(201).send({status: true, data: savedAuhorData});
-    } catch (err) {
+              res.status(201).send({status: true, data: savedAuhorData});
+              } catch (err) {
         res.status(500).send({status: false, msg: err.message});
     }
 };
 
+
+//===================================lOGIN  ||||||| AUTHOR===========================================
+
+
 let loginAuthor = async function (req,res) {
     try {
-        let email = req.headers.email;
-        let password = req.headers.password;
+       let {email,password} = req.body;
 
-        if(!email) return res.status(400).send({status: false, msg: 'please provide valid email id'});
+                if(!email) return res.status(400).send({status: false, msg: 'please provide valid email id'});
 
-        if(!password) return res.status(400).send({status: false, msg: 'please provide valid password'})
+                    if(!password) return res.status(400).send({status: false, msg: 'please provide valid password'})
 
-        let authors = await author.findOne({ email: email, password: password });
-        if(!authors) return res.send({
+                let authors = await author.findOne({ email: email, password: password });
+            if(!authors) return res.send({
         status: false, msg: "email or the password is not correct",
     });
 
     let token = jwt.sign(
         {
-            authorId: author._id,
-            project: 1,
-            group: "group-2",
+            authorId: author._id,               //
+                     project: 1,                //   PAYLOAD
+                         group: "group-2",      //
         },
-        "functionUp-project1"
+                              "functionUp-project1"   // SECRET KEY
     );
-    res.setHeader('x-api-key', token);
-    console.log(token);
-    res.status(201).send({ status: true, data: {token} });
-    } catch(err) {
-        res.status(500).send({ msg: "Error", msg: err.message })
+    res.setHeader('x-api-key', token);      // SETTING HEADER
+            console.log(token);
+                          return res.status(200).send({ status: true, data: {token} });
+           } catch(err) {
+   res.status(500).send({ msg: "Error", msg: err.message })
     }  
 };
 
-module.exports.loginAuthor = loginAuthor;
-module.exports.createAuthor = createAuthor;
+                         module.exports.loginAuthor = loginAuthor;
+                         module.exports.createAuthor = createAuthor;
